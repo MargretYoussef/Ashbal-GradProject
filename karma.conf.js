104515bdbd990c49401dea9c096536ce0ee41075
Margret Youssef Kamal <99531775+MargretYoussef@users.noreply.github.com>
2022-08-07 10:20:55 +0200

    two section in page home added

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ashbal-project'),
+      dir: require('path').join(__dirname, './coverage/ashbal-alfan'),
       subdir: '.',
       reporters: [
         { type: 'html' },
